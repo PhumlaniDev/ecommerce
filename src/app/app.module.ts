8221d22458d67e3d0c6d2fd6f466761b999e88ee
Phumlani <aphumlani.dev@gmail>
2023-03-29 14:07:12 +0200

    product info page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductInfoPageComponent } from './components/product-info-page/product-info-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductInfoPageComponent
   ],
   imports: [
     BrowserModule,
