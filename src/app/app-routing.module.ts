8745c94b3f138e440cbd898ed7f59a1381bd18f3
Phumlani <aphumlani.dev@gmail>
2023-04-01 22:05:40 +0200

    done product info page ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { ProductInfoPageComponent } from './components/product-info-page/product-info-page.component';
+
+const routes: Routes = [
+  {
+    path:"product",
+    component:ProductInfoPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
