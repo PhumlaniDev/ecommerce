0c78a3de9622c0629b5abbc9e2c2f50b15c57779
Advice <drnxumza@gmail.com>
2023-04-04 15:06:09 +0200

    Added bootstrap on my product

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class RegistrationComponent {
 
   title = "REGISTRATION PAGE"
+  public name ='Email.value'
+  public Surname='Surname.value'
+  public Email='Email.value'
+  public password='Email.value';
+  public jasmine_warning="class1"
+  Advice()
+  {
+
+    alert("Backend Process the following data")
+    this.title="new one"
+  }
+
 
 }
