fd4fd82a716dc73b8802dfebea2c4ef420c22023
Phumlani <aphumlani.dev@gmail.com>
2023-04-25 15:55:20 +0200

    fixing app module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
-import { ProductCardComponent } from './components/product-card/product-card.component';
 
-<<<<<<< HEAD
-const routes: Routes = [
-  {
-    path: "products",
-    component: ProductCardComponent
-  }
-=======
 import { NgModule } from '@angular/core';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductInfoPageComponent } from './components/product-info-page/product-info-page.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
@@ -21,7 +14,6 @@ const routes: Routes = [
     path:"products/:productId",
     component:ProductInfoPageComponent
   },
->>>>>>> 91f9783645b40a172670cb7fa5989b887d0ab73a
 ];
 
 @NgModule({
