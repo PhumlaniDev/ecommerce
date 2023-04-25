8c8cb7428baa7e9c44ec0a99fb1b630021671603
Phumlani <aphumlani.dev@gmail.com>
2023-04-25 15:52:49 +0200

    merge sihle's branch

--- app.module.ts
+++ app.module.ts
@@ -1,17 +1,14 @@
-import { AppComponent } from './src/app/app.component';
-import { AppRoutingModule } from './src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ProductInfoPageComponent } from './src/app/components/product-info-page/product-info-page.component';
-import { ProductListComponent } from './src/app/components/product-list/product-list.component';
+import { ProductInfoPageComponent } from 'src/app/components/product-info-page/product-info-page.component';
+import { ProductListComponent } from 'src/app/components/product-list/product-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 // import {CommonModule} from '@angular/common';
-
-
-
 
 @NgModule({
   declarations: [
